29074da5b2246da8034b63ff1bb12c328db8a316
minhduc <minhduc1994vcu@gmail.com>
2019-03-31 21:40:12 +0700

    add file

---
+++ src/main/java/FindMaxInTwoDimensionalArrays.java
@@ -0,0 +1,25 @@
+public class FindMaxInTwoDimensionalArrays {
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][5];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print(anInt + "\t");
+            }
+            System.out.println();
+        }
+        int max = matrix[0][0];
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                if (anInt > max) {
+                    max = anInt;
+                }
+            }
+        }
+        System.out.println("max in matrix is " + max);
+    }
+}
